354d5d89d1be61a204585e2ac1a4abf8d43886f8
dakshi goel <dakshie@dakshis-MacBook-Pro.local>
2025-07-11 17:21:17 +0530

    update: heading and subheading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
-// app/page.tsx (or Home.tsx)
 "use client";
 import WalletCard from "@/components/WalletCard";
 import BalanceDisplay from "@/components/BalanceDisplay";
 import TransferCard from "@/components/TransferCard";
-import { AvailWalletConnect } from "avail-wallet-sdk";
+import { AvailWalletConnect, useAvailAccount } from "avail-wallet-sdk";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const { selected } = useAvailAccount();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-8">
-      <h1 className="text-4xl font-bold mb-4">sdk demo</h1>
-      <p className="mb-8 text-center">Connect wallet using avail wallet sdk</p>
+      <h1 className="text-4xl font-bold mb-4">avail-connect sdk demo</h1>
+      <p className="mb-8 text-center">
+        {selected
+          ? "Connected wallet using avail-connect sdk"
+          : "Connect wallet using avail-connect sdk"}
+      </p>
 
       <AvailWalletConnect>
         <Button>Connect Wallet</Button>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,7 @@
-// app/providers.tsx
 "use client";
-import { AvailWalletProvider } from 'avail-wallet-sdk';
-import 'avail-wallet-sdk/index.css';
-
+import { AvailWalletProvider } from "avail-wallet-sdk";
+import "avail-wallet-sdk/index.css";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <AvailWalletProvider>
-      {children}
-    </AvailWalletProvider>
-  );
+  return <AvailWalletProvider>{children}</AvailWalletProvider>;
 }
